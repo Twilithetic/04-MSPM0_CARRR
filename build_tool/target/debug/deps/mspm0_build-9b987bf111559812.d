36a2d1d1960a7df0dabcd3627ac3b30b6e52260c
twilithetic <2934481106@qq.com>
2026-07-24 19:48:22 +0800

    chore: add .gitignore and remove tracked build artifacts

--- build_tool/target/debug/deps/mspm0_build-9b987bf111559812.d
+++
@@ -1,5 +0,0 @@
-D:\04-NUEDC_prepare\empty\build_tool\target\debug\deps\mspm0_build-9b987bf111559812.d: src\main.rs
-
-D:\04-NUEDC_prepare\empty\build_tool\target\debug\deps\mspm0_build-9b987bf111559812.exe: src\main.rs
-
-src\main.rs:
--- build_tool/target/debug/mspm0-build.d
+++
@@ -1,1 +0,0 @@
-D:\04-NUEDC_prepare\empty\build_tool\target\debug\mspm0-build.exe: D:\04-NUEDC_prepare\empty\build_tool\src\main.rs
