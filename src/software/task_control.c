568629f1276635a0aeb4aabc5ec0bd4c694664dc
Twilithetic <293448110^@qq.com>
2026-07-30 18:14:07 +0800

    循迹完全可以2

--- src/software/task_control.c
+++ src/software/task_control.c
@@ -12,6 +12,7 @@
 #include "include/app_tasks.h"
 #include "include/motor_driver_uart.h"
 #include "include/motor_driver_reg.h"
+#include "include/line8_reg.h"
 #include "include/XDS110_cdc.h"
 
 #include <FreeRTOS.h>
@@ -48,9 +49,17 @@ void vCarCtrlTask(void *pvParameters)
     float speed_xunhang = 200.0f;
 
     for (;;) {
-        g_line8_reg
+        uint8_t left_white_val = (uint8_t)(line8_get_line(0) * 2
+                      + line8_get_line(1) * 4
+                      + line8_get_line(2) * 8
+                      + line8_get_line(3) * 16);
+        
+        uint8_t right_white_val = (uint8_t)(line8_get_line(7) * 2
+                      + line8_get_line(6) * 4
+                      + line8_get_line(5) * 8
+                      + line8_get_line(4) * 16);
 
-        motor_send_speed_mm_s(target, target);
+        motor_send_speed_mm_s(speed_xunhang, speed_xunhang);
 
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
     }
